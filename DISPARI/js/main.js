7c610efbdb5ae8efcd5cf3311ef146fe79be5e97
Oleksandra Ubska <alessandraubska@gmail.com>
2024-06-12 17:18:54 +0200

    add function

--- DISPARI/js/main.js
+++ DISPARI/js/main.js
@@ -4,8 +4,11 @@ Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (u
 Dichiariamo chi ha vinto.
 */
 
-
-
+// seleziono il bottone gioca
+const gioca = document.getElementById("giocaBt");
+console.log(gioca);
+const ris = document.getElementById("risultato");
+console.log(risultato);
 
 
 
@@ -16,4 +19,10 @@ Dichiariamo chi ha vinto.
 function numeroCasuale() {
     return Math.floor(Math.random() * 5) + 1;
 }
-console.log(numeroCasuale);
+
+// funzione somma
+function somma(num1,num2) {
+    risultato = num1 + num2
+    return risultato
+}
+console.log(somma);
